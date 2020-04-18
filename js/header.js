b874f03df2a9dba10156269b8011a40c145c1c30
krystiano13 <krystiano13@interklasa.pl>
2020-04-18 13:53:19 +0200

    first commit

---
+++ js/header.js
@@ -0,0 +1,3 @@
+const nextBtn = document.querySelector('button'); // przycisk przejscia dalej
+const GoNext = () => window.location.href = "html/Choose.html"; // Przekierowanie dalej
+nextBtn.addEventListener('click',GoNext); // nasłuchiwacz czy przycisk jest kliknięty
