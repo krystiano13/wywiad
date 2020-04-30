065b5c3c45efd6524327a6b48ee9d74a7d9772be
krystiano13 <krystiano13@interklasa.pl>
2020-04-30 13:07:57 +0200

    'cjuyjuy'

--- js/rozmowa1.js
+++ js/rozmowa1.js
@@ -1,13 +1,16 @@
 const o1 = document.querySelector('.o1');
 const o2 = document.querySelector('.o2'); // okienka czatu
 const o3 = document.querySelector('.o3');
+const o4 = document.querySelector('.o4');
 
 const AddClasses1 = () => o1.classList.toggle('appear');
 const AddClasses2 = () => o2.classList.toggle('appear'); // Dodawanie klasy
 const AddClasses3 = () => o3.classList.toggle('appear');
+const AddClasses4 = () => o4.classList.toggle('appear');
 
 o1.addEventListener('transitionend',AddClasses2);
 o2.addEventListener('transitionend',AddClasses3);
+o3.addEventListener('transitionend',AddClasses4);
 
 setTimeout(AddClasses1,1000);
 
